dcbfae7bbab9e293fe4d24c66c3a7fe2b23405c4
unknown <alexdavisneuwiem@gmail.com>
2023-09-23 12:43:57 -0300

    Acho que essas são boas escolhas pro clipping

---
+++ src/math/clipping.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Clipping:
+    @staticmethod
+    def point_clippig():
+        pass
+
+    def cohen_sutherland():
+        pass
+
+    def nicholl_lee_nicholl():
+        pass
+
+    def weiler_atherton():
+        pass
